fbfeedd3a00d139975759a314e3d6dcb4f11f0c1
Anshuman <sanshuman4422@gmail.com>
2025-01-31 00:12:29 +0530

    2nd Commit Anshu

--- Dukandaar/src/main/java/com/mycompany/dukandaar/mongotest.java
+++ Dukandaar/src/main/java/com/mycompany/dukandaar/mongotest.java
@@ -19,49 +19,20 @@ import org.bson.conversions.Bson;
  */
 public class mongotest {
     public static void main(String args[])
-    {updateStockValues("RELIANCE",2800);
+    {String connectionString="mongodb+srv://Anshuman:CodeVimarsh@dukandaar.5ohzx.mongodb.net/?retryWrites=true&w=majority&appName=Dukandaar";
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            // Get database (MongoDB creates it if it doesn't exist)
+            MongoDatabase database = mongoClient.getDatabase("test");
+
+            // Run a simple command to check connection
+            Document ping = database.runCommand(new Document("ping", 1));
+            System.out.println("Connected successfully to MongoDB! Response: " + ping.toJson());
+        } catch (Exception e) {
+            System.err.println("Failed to connect to MongoDB: " + e.getMessage());
+        }
 
         
     }
-    public static void updateStockValues(String name, double newValue) {
-    // MongoDB Atlas connection string
-    String connectionString = "mongodb+srv://sanshuman4422:umangbsdk@cluster0.pi8si.mongodb.net/";
-
-    // Create a MongoDB client and connect to the database
-    try (MongoClient mongoClient = MongoClients.create(connectionString)) {
-        MongoDatabase database = mongoClient.getDatabase("BEARBULLZ"); 
-        MongoCollection<Document> collection = database.getCollection("STOCKS"); 
-
-        // Find the stock document by name
-        Document stock = collection.find(Filters.eq("NAME", name)).first();
-
-        if (stock != null) {
-            // Get the current values
-            double previousValue5 = stock.getDouble("VALUE5");
-            double previousValue4 = stock.getDouble("VALUE4");
-            double previousValue3 = stock.getDouble("VALUE3");
-            double previousValue2 = stock.getDouble("VALUE2");
-            double previousValue1 = stock.getDouble("VALUE1");
-
-            // Calculate the new CHANGE_PERC
-            double newChangePerc = ((newValue - previousValue5) / previousValue5) * 100;
-
-            // Create the updates
-            Bson updates = Updates.combine(
-                    Updates.set("VALUE5", newValue),          // New current value
-                    Updates.set("VALUE4", previousValue5),    // Previous VALUE5 becomes VALUE4
-                    Updates.set("VALUE3", previousValue4),    // Previous VALUE4 becomes VALUE3
-                    Updates.set("VALUE2", previousValue3),    // Previous VALUE3 becomes VALUE2
-                    Updates.set("VALUE1", previousValue2),    // Previous VALUE2 becomes VALUE1
-                    Updates.set("CHANGE_PERC", newChangePerc) // Update CHANGE_PERC
-            );
-
-            collection.updateOne(Filters.eq("NAME", name), updates);
-            System.out.println("Updated stock: " + name);
-        } else {
-            System.out.println("Stock not found: " + name);
-        }
-    }
-}
+   
     
 }
