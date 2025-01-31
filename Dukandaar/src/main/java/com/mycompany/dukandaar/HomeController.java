458be91e7e31c532659cee95e9f8c1c6bbad21ee
sarthak1609 <sarthak1suryavanshi16@gmail.com>
2025-01-31 23:13:42 +0530

    near final

--- Dukandaar/src/main/java/com/mycompany/dukandaar/HomeController.java
+++ Dukandaar/src/main/java/com/mycompany/dukandaar/HomeController.java
@@ -14,6 +14,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -22,14 +25,37 @@ import javafx.stage.Stage;
  * @author Om
  */
 public class HomeController  {
-
     /**
      * Initializes the controller class.
      */
     Stage stage;
+    @FXML
+    private VBox wholesalers;
     void setStage(Stage stage)
-    {
+    {    
+    show();
         this.stage=stage;
-    }    
+    }  
+    public void show(){
+        int i=1;
+        for (String key : Auth.getTopWholesalers()) {
+            System.out.println(key);
+            wholesalers.getChildren().add(hb(i++,key));
+        }
+    }
+    private HBox hb(int i,String s){
+        HBox card = new HBox();
+        card.getStyleClass().add("user-card");
+        card.setPrefWidth(600);
+        
+        Label indexLabel = new Label(String.valueOf(i));
+        indexLabel.getStyleClass().add("index-number");
+        
+        Label usernameLabel = new Label(s);
+        usernameLabel.getStyleClass().add("username");
+        
+        card.getChildren().addAll(indexLabel, usernameLabel);
+        return card;
+    }
     
 }
