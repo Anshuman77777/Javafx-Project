43aa3ff99cf246987d211f641e00d1c93bbbf1da
sarthak1609 <sarthak1suryavanshi16@gmail.com>
2025-01-31 13:55:04 +0530

    sarthak

--- Dukandaar/src/main/java/com/mycompany/dukandaar/retailerpanelcontroller.java
+++ Dukandaar/src/main/java/com/mycompany/dukandaar/retailerpanelcontroller.java
@@ -4,11 +4,15 @@
  */
 package com.mycompany.dukandaar;
 
+
+
+import java.awt.Canvas;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -16,12 +20,32 @@ import javafx.stage.Stage;
  * @author Anshu
  */
 public class retailerpanelcontroller {
+    @FXML
+    BorderPane Canvas;
     Stage stage;
     void setStage(Stage stage)
     {
         this.stage=stage;
         
     }
+    
+    @FXML
+    void home()throws IOException
+    {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("home.fxml"));
+        Parent root=loader.load();
+        HomeController hpc = loader.getController();
+        
+        
+        hpc.setStage(stage);
+    // Assuming Canvas is a BorderPane (or another Pane type)
+        Canvas.setCenter(root);
+        Canvas.getStylesheets().add(getClass().getResource("home.css").toExternalForm());
+         
+         
+
+    }
+    
     @FXML
     void logout() throws IOException
     {
